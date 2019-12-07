5c3d066277e349c4eae7210ae5c775f04b184f74
totalchest <13sergey78@mail.ru>
2019-12-07 13:40:40 +0300

    Lab 2 done

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -117,7 +117,7 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 	info->eip_fn_narg = 0;
 
 	// Find the relevant set of stabs
-	if (addr >= ULIM) {
+	if (addr < ULIM) {
 		stabs = __STAB_BEGIN__;
 		stab_end = __STAB_END__;
 		stabstr = __STABSTR_BEGIN__;
@@ -179,8 +179,17 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 	//	Look at the STABS documentation and <inc/stab.h> to find
 	//	which one.
 	// Your code here.
-
-
+	stab_binsearch(stabs, &lline, &rline, N_SLINE, addr);
+	if (lline <= rline)
+	{
+		info -> eip_line = stabs[lline].n_desc;
+	}
+	else
+	{
+		return -1;
+	}
+	
+	
 	// Search backwards from the line number for the relevant filename
 	// stab.
 	// We can't just use the "lfile" stab because inlined functions
