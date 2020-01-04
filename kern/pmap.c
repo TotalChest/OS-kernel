99627b0e218c228d055534b3881b92984b7ebdfb
totalchest <13sergey78@mail.ru>
2020-01-04 17:38:14 +0300

    Lab 12 done

--- kern/pmap.c
+++ kern/pmap.c
@@ -184,6 +184,7 @@ mem_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'vsys' point to an array of size 'NVSYSCALLS' of int.
 	// LAB 12: Your code here.
+	vsys = boot_alloc(NVSYSCALLS * sizeof(*vsys));
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -225,6 +226,7 @@ mem_init(void)
 	//    - the new image at UVSYS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 12: Your code here.
+	boot_map_region(kern_pgdir, UVSYS, ROUNDUP(sizeof(*vsys) * NVSYSCALLS, PGSIZE), PADDR(vsys), PTE_U | PTE_P);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
