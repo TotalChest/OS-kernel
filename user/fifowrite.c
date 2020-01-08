4e3bd9e571db767223482c1a0714563b8f70971e
totalchest <13sergey78@mail.ru>
2020-01-08 12:08:01 +0300

    Mod fifowrite, Add DedcribeFIFO

--- user/fifowrite.c
+++ user/fifowrite.c
@@ -1,17 +1,24 @@
 #include <inc/lib.h>
 
 void
+usage(void)
+{
+	cprintf("usage: fifowrite [FIFO] [word]\n");
+	exit();
+}
+
+void
 umain(int argc, char **argv)
 {
 
 	int fd;
-	
-	char string[] = "Hello JOS kernel!";
+
+	if (argc != 3)
+		usage();
 
 	if ((fd = open(argv[1], O_WRONLY)) < 0)
 		printf("Open error");
 
-	if (write(fd, string, sizeof(string)) == sizeof(string))
-		printf("Write success");
+	printf("Write %d: \n", fprintf(fd, "%s", argv[2]));
 
 }
